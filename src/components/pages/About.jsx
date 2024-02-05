04962d666a5e8b71ec5fe651b5b147fac71e7422
Meep Dong <dmossmaurice@gmail.com>
2024-02-05 16:27:47 +0530

    commit 3

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './modules.css';
+
+export const About = () => {
+  return (
+  <>
+  <h1>About</h1>
+  <section className="Section1"></section>
+  <div className="illustration"></div>
+  </>
+  )
+};
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './modules.css';
+
+export const Home = () => {
+  return (
+  <>
+  <h1>Home</h1>
+  </>
+  )
+};
